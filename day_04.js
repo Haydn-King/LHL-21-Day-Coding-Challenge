d4bfe37672e5f65c8ada8433d4d6d9bbd9db928d
Haydn King <ahmillikin@gmail.com>
2021-11-26 01:04:51 -0800

    add day4 and day5

--- day_04.js
+++ day_04.js
@@ -1,6 +1,6 @@
-// ===========================================
+// =====================================================
 // Challenge #4 - Hope you’re not afraid of the dark...
-// ===========================================
+// =====================================================
 
 // Create a function that takes in a toggle object and will change the value of the property isOn between true and false and return the updated object. Using the function twice should revert the toggle back to its original value.
 
